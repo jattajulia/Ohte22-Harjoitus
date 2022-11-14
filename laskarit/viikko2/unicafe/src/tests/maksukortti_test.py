fd1dde80532fdfa1f99aa4dfe2e383b0edf2bcb9
jattajulia <jatta.kivihaka@helsinki.fi>
2022-11-14 18:10:59 +0200

    add new test

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
@@ -7,3 +8,7 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
